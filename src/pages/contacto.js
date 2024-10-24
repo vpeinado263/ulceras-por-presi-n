192a48323bf0f788ece0f88b584f65a07f9ac7a4
Victor Peinado <vpeinado263@gmail.com>
2024-10-24 20:06:36 -0300

    Cambiar primera letra del nombre la pagina dentro del codigo

--- src/pages/contacto.js
+++ src/pages/contacto.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const contacto = () => {
+const Contacto = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const contacto = () => {
   )
 }
 
-export default contacto
+export default Contacto
