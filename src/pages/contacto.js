7990386004ab1d5a2b120df3973b4ff1406e590c
Victor Peinado <vpeinado263@gmail.com>
2024-10-24 20:03:33 -0300

    Crear paginas de contacto y servicios

---
+++ src/pages/contacto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contacto = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default contacto
---
+++ src/pages/servicios.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const servicios = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default servicios
