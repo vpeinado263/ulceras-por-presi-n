cf67913b4c305b54a6f17861d1e5b7d014b77e34
Victor Peinado <vpeinado263@gmail.com>
2024-10-25 01:57:26 -0300

    Finalizar componente padre para los estado de contador y posicion

--- src/components/SectionUpp.js
+++ src/components/SectionUpp.js
@@ -1,9 +1,28 @@
 import { useState } from "react"
+import ContadorCambios from "./Contador";
+import Posicion from "./Posicion";
 
 const SectionUpp = () => {
+  const initialPosicion = "Decubito supino";
+  const [posicion, setPosicion] = useState(initialPosicion);
+  const initialContador = 0;
+  const [contador, setContador] = useState(initialContador);
+
+  const cambiarPosicion = (nuevaPosicion) => {
+    setPosicion(nuevaPosicion);
+    setContador((prev) => +1);
+  };
+
+  const resetearContador = () => {
+    setPosicion(initialPosicion);
+    setContador(initialContador);
+  };
+
   return (
     <div>
-      
+      <h1>Cambios posturales del paciente</h1>
+      <Posicion posicion={posicion} cambiarPosicion={cambiarPosicion}/>
+      <ContadorCambios contador={contador} resetearContador={resetearContador}/>
     </div>
   )
 }
