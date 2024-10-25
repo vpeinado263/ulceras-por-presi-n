e32518d7f221be87c151c851d19e7d6e7dae509f
Victor Peinado <vpeinado263@gmail.com>
2024-10-25 00:03:34 -0300

    Crear componente posicion con useState

---
+++ src/components/Posicion.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Posicion = () => {
+    const [posicion, setPosicion] = useState("Decubito supino")
+  return (
+    <>
+    <h3>Posición Actual: {posicion}</h3>
+    <button onClick={() => setPosicion("Decúbito Lateral Izquierdo")}>Cambiar a Lateral Izquierdo</button>
+    <button onClick={() => setPosicion("Decúbito Lateral Derecho")}>Cambiar a lateral Derecho</button>
+    <button onClick={() => setPosicion("Decúbito Supiono")}>Volver a supino</button>
+    </>
+  )
+}
+
+export default Posicion
