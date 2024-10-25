60cd2fc3a97c611819e1967a8aba462c38588798
Victor Peinado <vpeinado263@gmail.com>
2024-10-24 22:05:26 -0300

    Incorporar base de datos para props de Tarjetas

--- src/components/CardsContainer.js
+++ src/components/CardsContainer.js
@@ -1,6 +1,35 @@
 import Card from "./Card"
 
-const x = [1, 2, 3, 4]
+const productos = [
+    {
+        id: 1,
+        img: "https://http2.mlstatic.com/D_NQ_NP_716492-MLA52853049595_122022-O.webp",
+        title:  "Colchones de aire o espuma",
+        text: "Para reducir la presión en las áreas vulnerables.",
+        bgcolor: "red",
+    },
+    {
+        id: 2,
+        img: "https://botiquinsans.com/wp-content/uploads/2023/03/Aposito_hidrocoloide_comfeel_plus_20_cm_x_20_cm_10-300x300.jpg",
+        title:  "Apósitos para heridas",
+        text: "Apósito hidrocoloide: Mantiene un ambiente húmedo para la cicatrización.",
+        bgcolor: "red",
+    },
+    {
+        id: 3,
+        img: "https://techdent.cl/wp-content/uploads/2021/06/Suero-braun-250ml.jpg",
+        title:  "Soluciones limpiadoras:",
+        text: "Suero fisiológico (solución salina estéril): Para limpiar la herida.",
+        bgcolor: "red",
+    },
+      {
+        id: 4,
+        img: "https://prevencionulcerasyheridas.com/wp-content/uploads/2021/10/Almohadilla-sacro.jpg",
+        title:  "Materiales de fijación y sujeción:",
+        text: "Gasas estériles: Para cubrir heridas y aplicar apósitos.",
+        bgcolor: "red",
+      }
+  ];
 
 const CardContainer = () => {
     return (
